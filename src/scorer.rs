1fec2c7e5de11a3860bb047d3c0466d5f803cb2c
Trevor McCulloch <mccullocht@gmail.com>
2024-06-19 22:05:28 -0700

    Greatly speed scoring for packed scalar vectors.
    Load greater amounts at once: load 128 bits in all cases which unpacks
    to 64 elements (2-bit) or 32 elements (4-bit).
    
    Unpack the query just once when using a QueryScorer instead of unpacking
    for each score() call. This is only applied in the search path but could
    easily help in the build path.

--- src/scorer.rs
+++ src/scorer.rs
@@ -62,8 +62,8 @@ impl QuantizedEuclideanScorerState {
             Self::Binary => HammingScorer.score(a, b),
             Self::Scalar(bits, scoring_adjustment_multiplier) => {
                 let distance = match bits {
-                    2 => Self::distance2_doubleword(a, b),
-                    4 => Self::distance4_doubleword(a, b),
+                    2 => Self::distance2(a, b),
+                    4 => Self::distance4(a, b),
                     8 => unsafe {
                         SpatialSimilarity::l2sq(
                             std::slice::from_raw_parts(a.as_ptr() as *const i8, a.len()),
@@ -79,50 +79,107 @@ impl QuantizedEuclideanScorerState {
         }
     }
 
-    fn distance2_doubleword(a: &[u8], b: &[u8]) -> f64 {
-        let chunk_to_buf = |c: &[u8]| -> [i8; 16] {
-            let word = if c.len() == 4 {
-                u32::from_le_bytes(c.try_into().unwrap())
-            } else {
-                let mut b = [0u8; 4];
-                b[..c.len()].copy_from_slice(c);
-                u32::from_le_bytes(b)
-            };
-            let wbuf = [
-                word & 0x03030303,
-                (word >> 2) & 0x03030303,
-                (word >> 4) & 0x03030303,
-                (word >> 6) & 0x03030303,
-            ];
-            unsafe { std::mem::transmute(wbuf) }
+    fn score_unpacked(&self, unpacked: &[i8], packed: &[u8]) -> NotNan<f32> {
+        let distance = match self {
+            Self::Binary => unreachable!(),
+            Self::Scalar(bits, score_adjustment_multiplier) => {
+                let raw_distance: f64 = match bits {
+                    2 => unpacked
+                        .chunks(64)
+                        .zip(packed.chunks(16))
+                        .map(|(u, p)| {
+                            let buf = Self::unpack2_quad(p);
+                            SpatialSimilarity::l2sq(u, &buf).unwrap()
+                        })
+                        .sum(),
+                    4 => unpacked
+                        .chunks(32)
+                        .zip(packed.chunks(16))
+                        .map(|(u, p)| {
+                            let buf = Self::unpack4_quad(p);
+                            SpatialSimilarity::l2sq(u, &buf).unwrap()
+                        })
+                        .sum(),
+                    _ => unreachable!(),
+                };
+                raw_distance as f32 * *score_adjustment_multiplier
+            }
         };
-        a.chunks(8)
-            .zip(b.chunks(8))
+        NotNan::new(1.0 / (1.0 + distance)).unwrap()
+    }
+
+    fn unpack_vector(&self, packed: &[u8]) -> Option<Vec<i8>> {
+        match self {
+            Self::Binary => None,
+            Self::Scalar(bits, _) => match bits {
+                2 => Some(
+                    packed
+                        .chunks(16)
+                        .flat_map(|p| Self::unpack2_quad(p))
+                        .collect(),
+                ),
+                4 => Some(
+                    packed
+                        .chunks(16)
+                        .flat_map(|p| Self::unpack4_quad(p))
+                        .collect(),
+                ),
+                _ => None,
+            },
+        }
+    }
+
+    fn unpack2_quad(packed: &[u8]) -> [i8; 64] {
+        debug_assert!(packed.len() <= 16);
+        let quad = if packed.len() == 16 {
+            u128::from_le_bytes(packed.try_into().unwrap())
+        } else {
+            let mut b = [0u8; 16];
+            b[..packed.len()].copy_from_slice(packed);
+            u128::from_le_bytes(b)
+        };
+        let mask = u128::from_le_bytes([0x3u8; 16]);
+        let wbuf = [
+            quad & mask,
+            (quad >> 2) & mask,
+            (quad >> 4) & mask,
+            (quad >> 6) & mask,
+        ];
+        // Safety: array-to-array conversion of a primitive type.
+        unsafe { std::mem::transmute(wbuf) }
+    }
+
+    fn unpack4_quad(packed: &[u8]) -> [i8; 32] {
+        let quad = if packed.len() == 16 {
+            u128::from_le_bytes(packed.try_into().unwrap())
+        } else {
+            let mut b = [0u8; 16];
+            b[..packed.len()].copy_from_slice(packed);
+            u128::from_le_bytes(b)
+        };
+        let mask = u128::from_le_bytes([0xfu8; 16]);
+        let wbuf = [quad & mask, (quad >> 4) & mask];
+        // Safety: array-to-array conversion of a primitive type.
+        unsafe { std::mem::transmute(wbuf) }
+    }
+
+    fn distance2(a: &[u8], b: &[u8]) -> f64 {
+        a.chunks(16)
+            .zip(b.chunks(16))
             .map(|(ac, bc)| {
-                let abuf = chunk_to_buf(ac);
-                let bbuf = chunk_to_buf(bc);
+                let abuf = Self::unpack2_quad(ac);
+                let bbuf = Self::unpack2_quad(bc);
                 SpatialSimilarity::l2sq(&abuf, &bbuf).unwrap()
             })
             .sum()
     }
 
-    fn distance4_doubleword(a: &[u8], b: &[u8]) -> f64 {
-        let chunk_to_buf = |c: &[u8]| -> [i8; 16] {
-            let word = if c.len() == 8 {
-                u64::from_le_bytes(c.try_into().unwrap())
-            } else {
-                let mut b = [0u8; 8];
-                b[..c.len()].copy_from_slice(c);
-                u64::from_le_bytes(b)
-            };
-            let wbuf = [word & 0x0f0f0f0f0f0f0f0f, (word >> 4) & 0x0f0f0f0f0f0f0f0f];
-            unsafe { std::mem::transmute(wbuf) }
-        };
-        a.chunks(8)
-            .zip(b.chunks(8))
+    fn distance4(a: &[u8], b: &[u8]) -> f64 {
+        a.chunks(16)
+            .zip(b.chunks(16))
             .map(|(ac, bc)| {
-                let abuf = chunk_to_buf(ac);
-                let bbuf = chunk_to_buf(bc);
+                let abuf = Self::unpack4_quad(ac);
+                let bbuf = Self::unpack4_quad(bc);
                 SpatialSimilarity::l2sq(&abuf, &bbuf).unwrap()
             })
             .sum()
@@ -136,6 +193,21 @@ pub struct QuantizedEuclideanScorer(QuantizedEuclideanScorerState);
 impl QuantizedEuclideanScorer {
     pub fn new(quantizer: &Quantizer) -> Self {
         Self(QuantizedEuclideanScorerState::new(quantizer))
+    }
+
+    /// If the vector packs multiple elements per byte, unpack them to produce a vector with one
+    /// element per byte.
+    ///
+    /// Returns `None` if thie quantizer does not produce packed vectors.
+    fn unpack_vector(&self, packed: &[u8]) -> Option<Vec<i8>> {
+        self.0.unpack_vector(packed)
+    }
+
+    /// Score an unpacked vector against a packed vector.
+    /// If the quantizer packs multiple elements into a single byte this may elide a bunch of work
+    /// by only unpacking the query once.
+    fn score_unpacked(&self, unpacked: &[i8], packed: &[u8]) -> NotNan<f32> {
+        self.0.score_unpacked(unpacked, packed)
     }
 }
 
@@ -189,6 +261,7 @@ where
 pub struct QuantizedEuclideanQueryScorer {
     scorer: QuantizedEuclideanScorer,
     query: Vec<u8>,
+    unpacked: Option<Vec<i8>>,
 }
 
 impl QuantizedEuclideanQueryScorer {
@@ -196,7 +269,12 @@ impl QuantizedEuclideanQueryScorer {
     pub fn new(quantizer: &Quantizer, query: &[f32]) -> Self {
         let scorer = QuantizedEuclideanScorer::new(quantizer);
         let query = quantizer.quantize(query);
-        Self { scorer, query }
+        let unpacked = scorer.unpack_vector(&query);
+        Self {
+            scorer,
+            query,
+            unpacked,
+        }
     }
 }
 
@@ -204,7 +282,11 @@ impl QueryScorer for QuantizedEuclideanQueryScorer {
     type Vector = [u8];
 
     fn score(&self, a: &Self::Vector) -> NotNan<f32> {
-        self.scorer.score(self.query.as_ref(), a)
+        if let Some(unpacked) = self.unpacked.as_ref() {
+            self.scorer.score_unpacked(unpacked, a)
+        } else {
+            self.scorer.score(self.query.as_ref(), a)
+        }
     }
 }
 
